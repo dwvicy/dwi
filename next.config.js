9eca44fcff47671a816d9b9e08951a6ea3899ec9
dwivi <vaishdw2207@gmail.com>
2020-09-24 21:27:05 +0530

    fix: certain changes

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
 // const debug = process.env.NODE_ENV !== 'production'
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-module.exports = withPlugins([
-  [optimizedImages, {
-    mozjpeg: {
-      quality: 80,
-    },
-    pngquant: {
-      speed: 3,
-      strip: true,
-      verbose: true,
-    },
-    imagesPublicPath: '/dwi/_next/static/images/',
-  }],
-  {
-    basePath: '/dwi',
-    assetPrefix: '/dwi/',
-    env,
-  },
-]);
+module.exports = {
+  assetPrefix: '/dwi/' ,
+  basePath: '/dwi'
+}
