1c3e0f5a6053a5dfa2c9309b45891051a78243bb
dwivi <vaishdw2207@gmail.com>
2020-10-04 15:38:04 +0530

    improvising index.js

--- pages/index.js
+++ pages/index.js
@@ -9,42 +9,6 @@ export default function Home() {
         <title>dwi</title>
         <a rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hello, Vaishnavi here (⌐■_■)!
-        </h1>
-
-        <p className={styles.description}>
-         If you haven't watched fight club, plis leave now!
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://github.com/dwvicy" className={styles.card}>
-            <a><h3>Stuff I built &rarr;</h3></a>
-          </a>
-
-          <a href="https://medium.com/@vaishdw2207" className={styles.card}>
-            <a><h3>Stuff I wrote &rarr;</h3></a>
-            
-          </a>
-
-          <a
-            href="https://www.linkedin.com/in/vaishnavi-dwivedi-0b4207114/"
-            className={styles.card}
-          >
-            <a><h3>Stuff I achieved &rarr;</h3></a>
-          </a>
-
-          <a
-            href="https://vaishnavidwivedi.me/developers-guide"
-            className={styles.card}
-          >
-            <a><h3>Developers' Guide &rarr;</h3></a>
-          </a>
-        </div>
-      </main>
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
